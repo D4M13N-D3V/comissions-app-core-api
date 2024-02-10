db4aaee09b9ac74742a7935c1c89e1f55a33ed7c
Damien Ostler <damien.ostler@data443.com>
2024-02-10 17:11:46 -0500

    added update api call

---
+++ src/comissions.app.api/Controllers/Admin/AdminDatabaseController.cs
@@ -0,0 +1,26 @@
+using comissions.app.database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace comissions.app.api.Controllers;
+
+[ApiController]
+[Authorize("admin")]
+[Route("api/admin/[controller]")]
+public class AdminDatabaseController:ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public AdminDatabaseController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    [HttpPatch]
+    public async Task<IActionResult> UpdateDatabase()
+    {
+        await _dbContext.Database.MigrateAsync();
+        return Ok();
+    }
+}
