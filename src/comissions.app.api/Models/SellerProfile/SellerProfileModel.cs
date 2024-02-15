5a5497a93b4e8ca2a05c9dc6ea67fc6e71b85d59
Damien Ostler <damien.ostler@data443.com>
2024-02-14 21:58:51 -0500

    fix: api now returns Id of seller in the response for get seller profile

--- src/comissions.app.api/Models/SellerProfile/SellerProfileModel.cs
+++ src/comissions.app.api/Models/SellerProfile/SellerProfileModel.cs
@@ -2,6 +2,7 @@ namespace comissions.app.api.Models.SellerProfile;
 
 public class SellerProfileModel
 {
+    public int Id { get; set; }
     public List<string> SocialMediaLinks { get; set; }
     public string Biography { get; set; }
     public bool PrepaymentRequired { get; set; }
