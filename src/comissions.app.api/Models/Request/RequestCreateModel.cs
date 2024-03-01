823289452b3797462350ef81775ef310006a2766
Damien Ostler <damien.ostler@data443.com>
2024-02-29 20:47:47 -0500

    fix

--- src/comissions.app.api/Models/Request/RequestCreateModel.cs
+++ src/comissions.app.api/Models/Request/RequestCreateModel.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace comissions.app.database.Models.Request;
 
 public class RequestCreateModel
 {
-    public int ArtistId { get; set; }
-    public string Message { get; set; }
-    public decimal Amount { get; set; }
+    [FromForm]public int ArtistId { get; set; }
+    [FromForm]public string Message { get; set; }
+    [FromForm]public decimal Amount { get; set; }
     
-    public List<IFormFile> Files { get; set; }
+    [FromForm]public List<IFormFile> Files { get; set; }
 }
