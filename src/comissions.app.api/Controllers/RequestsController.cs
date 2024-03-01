fe37a5ae7df338e972215b3877a45b91b034e52a
Damien Ostler <damien.ostler@data443.com>
2024-02-29 20:32:01 -0500

    Revert "got rid of model"
    This reverts commit ca2bef2ba971a2708808293404e0306e010dcb6d.

--- src/comissions.app.api/Controllers/RequestsController.cs
+++ src/comissions.app.api/Controllers/RequestsController.cs
@@ -1054,21 +1054,16 @@ public class RequestsController : Controller
         return Ok(result);
     }
 
-    // public int ArtistId { get; set; }
-    // public string Message { get; set; }
-    // public decimal Amount { get; set; }
-    //
-    // public List<IFormFile> Files { get; set; }
     [Authorize("write:request")]
     [HttpPost]
     [Route("Request")]
-    public async Task<IActionResult> CreateRequest([FromForm]int artistId, [FromForm]string message, [FromForm]decimal amount, [FromForm]List<IFormFile> files)
+    public async Task<IActionResult> CreateRequest([FromForm] RequestCreateModel model)
     {
         var openRequests = await _dbContext.Requests
             .Where(x=>x.UserId==User.GetUserId() && x.Declined==false && x.Completed==false)
             .CountAsync();
         
-        var artist = await _dbContext.UserArtists.FirstOrDefaultAsync(x=>x.Id==artistId);
+        var artist = await _dbContext.UserArtists.FirstOrDefaultAsync(x=>x.Id==model.ArtistId);
         if(artist==null)
             return NotFound("Artist not found.");
         
@@ -1077,11 +1072,11 @@ public class RequestsController : Controller
         var userId = User.GetUserId();
         var request = new Request()
         {
-            Amount = amount,
-            Message = message,
+            Amount = model.Amount,
+            Message = model.Message,
             RequestDate = DateTime.UtcNow,
             UserId = userId,
-            ArtistId = artistId,
+            ArtistId = model.ArtistId,
             Accepted = false,
             AcceptedDate = null,
             Declined = false,
@@ -1102,7 +1097,7 @@ public class RequestsController : Controller
         };
         
         var references = new List<RequestReference>();
-        foreach (var file in files)
+        foreach (var file in model.Files)
         {
             var reference = new RequestReference()
             {
