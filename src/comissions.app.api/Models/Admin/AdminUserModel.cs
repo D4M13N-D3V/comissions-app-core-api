f5a5b13ddf5b56a8aae2bb87e0cff9ca0d4f7952
Damien Ostler <damien.ostler@data443.com>
2024-03-17 05:08:25 -0400

    fix: added model for admin users endpoint

---
+++ src/comissions.app.api/Models/Admin/AdminUserModel.cs
@@ -0,0 +1,35 @@
+using comissions.app.database.Entities;
+
+namespace comissions.app.database.Models.Admin;
+
+public class AdminUserModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string DisplayName { get; init; } = string.Empty;
+    public string Biography { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public int NumberOfRequests { get; set; } = 0;
+    public int NumberOfSuspensions { get; set; } = 0;
+    public int NumberOfBans { get; set; } = 0;
+    public int NumberOfReviews { get; set; } = 0;
+    public decimal AmountSpent { get; set; } = 0;
+}
+
+public static class AdminUserModelExtensions
+{
+    public static AdminUserModel ToAdminUserModel(this User user)
+    {
+        return new AdminUserModel
+        {
+            Id = user.Id,
+            DisplayName = user.DisplayName,
+            Biography = user.Biography,
+            Email = user.Email,
+            NumberOfRequests = user.Requests.Count,
+            NumberOfSuspensions = user.Suspensions.Count,
+            NumberOfBans = user.Bans.Count,
+            NumberOfReviews = user.Requests.Count,
+            AmountSpent = user.Requests.Sum(r => r.Amount)
+        };
+    }
+}
