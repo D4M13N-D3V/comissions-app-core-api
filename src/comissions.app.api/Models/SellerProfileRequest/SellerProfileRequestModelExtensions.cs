543297cc2a79ffe5e2a999fb2fb048c88fc433fe
Damien Ostler <damien.ostler@data443.com>
2024-03-17 04:19:37 -0400

    fix: the model was not being populated with the user data properly with extension method

--- src/comissions.app.api/Models/SellerProfileRequest/SellerProfileRequestModelExtensions.cs
+++ src/comissions.app.api/Models/SellerProfileRequest/SellerProfileRequestModelExtensions.cs
@@ -1,3 +1,5 @@
+using comissions.app.api.Models.User;
+
 namespace comissions.app.api.Models.ArtistRequest;
 
 public static class ArtistRequestModelExtensions
@@ -8,6 +10,7 @@ public static class ArtistRequestModelExtensions
         {
             Id = sellerProfileRequest.Id,
             UserId = sellerProfileRequest.UserId,
+            User = sellerProfileRequest.User.ToModel(),
             RequestDate = sellerProfileRequest.RequestDate,
             Accepted = sellerProfileRequest.Accepted,
             Message = sellerProfileRequest.Message
